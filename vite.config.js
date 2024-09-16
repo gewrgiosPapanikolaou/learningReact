3bf47ccea8c501c9023057c986bbb8ebba2b5be2
George Papanikolaou <gewrgios.papanikolaou+1@gmail.com>
2024-09-16 05:51:48 -0400

    add package.json,vite.config modified files

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig({ 
+   base: "/learning_react",      
     plugins: [react()],
     server: {
       proxy: {
